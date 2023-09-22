3ae8638df3fbc3b83636cb0a912b3dde3e20e781
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-22 15:30:48 +0530

    second

--- Subroutines.java
+++ Subroutines.java
@@ -1,5 +1,25 @@
 public class Subroutines {
     public static void main(String[] args) {
+        long startTime;
+        long endTime;
+        long diffTime;
+
+        startTime = System.nanoTime();
         
+        double width;
+        double height;
+        double hypotenus;
+
+        width = 10;
+        height = 20;
+
+        hypotenus = Math.sqrt(width*width + height*height);
+        System.out.println("hypotenus of the traingle is: " + Math.round(hypotenus));
+
+        endTime = System.nanoTime();
+        diffTime = endTime - startTime;
+        System.out.println(diffTime/1000000000.00);
+        
+
     }
 }
