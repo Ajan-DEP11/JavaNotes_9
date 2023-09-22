6e336bc902f851b11b45b9e1492fdb812dbf3180
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-22 19:57:06 +0530

    fourth commit

--- GetInput.java
+++ GetInput.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class GetInput {
+    private static final Scanner scaanner = new Scanner(System.in);
     public static void main(String[] args) {
+
+        int userInput;
+        int square;
+
+        System.out.print("Enter a number: ");
+        userInput = scaanner.nextInt();
+        scaanner.nextLine();
+
+        square = userInput*userInput;
+
+        System.out.printf("Square value of the %s is %s : ",userInput,square);
+
+       
+
         
     }
 }
