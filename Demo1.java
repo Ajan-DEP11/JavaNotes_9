4e06cd7083d119898f5125f51b352f4a8c611022
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-29 22:14:14 +0530

    Add second

--- Demo1.java
+++ Demo1.java
@@ -1,3 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Demo1 {
+    public static void main(String[] args) {
+         String text1 = "My Customer id: C-123";
+        String text2 = "C-450";
+
+        Pattern compile = Pattern.compile("C-\\d{3}");
+        Matcher matcher = compile.matcher(text1);
+        Matcher matcher2 = compile.matcher(text2);
+
+       /*1 */ System.out.println(matcher.matches());
+            matcher.reset();
+            System.out.println(matcher.find());
+            
+    }
     
 }
