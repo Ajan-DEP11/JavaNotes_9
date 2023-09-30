e1d9ea11735b4a2180fea989b935e7d4113928d0
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-30 09:29:00 +0530

    Final commit

--- WhileLoop.java
+++ WhileLoop.java
@@ -12,11 +12,30 @@ public class WhileLoop {
         sum=0;
         count=0;
 
-        System.out.println("Enter the Integer Number: ");
+        System.out.print("Enter the Integer Number: ");
         input  = scanner.nextInt();
         scanner.nextLine();
 
+        while(input !=0){
+            sum += input;
+            count++;
+           
+             System.out.print("Enter a Integer, if you want to end the cal enter 0:");
+             input = scanner.nextInt();
+             scanner.nextLine();
+        }
+        if(input ==0){
+            System.out.println("Programe is ended..");
+        }
+         Average = (double)sum/count;
+         System.out.printf("average is %.2f: \n",Average);
+      
         
+       
+        
+
+
+
 
 
     }
