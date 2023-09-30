36f1e70cd8f627fb407f3888d4f60be77f73d1f0
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-30 18:23:45 +0530

    Final commit

--- Demo2.java
+++ Demo2.java
@@ -1,6 +1,11 @@
 public class Demo2 {
     public static void main(String[] args) {
-        
+        int N;
+        int D;
+        N=10;
+        D=3;
+        System.out.println(N/D);
+        System.out.println(D/N);
     }
     
 }
