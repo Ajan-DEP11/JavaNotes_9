f9cc68a91ebf63deb32c54dcb006410054a40a47
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-30 08:54:55 +0530

    Add Scanner

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class WhileLoop {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         
+        int input;
+        int sum;
+        int count;
+        double Average;
+
+        sum=0;
+        count=0;
+
+        System.out.println("Enter the Integer Number: ");
+        input  = scanner.nextInt();
+        scanner.nextLine();
+
+        
+
+
     }
 }
