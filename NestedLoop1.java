d7bce86202fa126eb8bcbe32d50b4d72da7c324b
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-09-30 19:24:07 +0530

    last commit

--- NestedLoop1.java
+++ NestedLoop1.java
@@ -1,5 +1,13 @@
 public class NestedLoop1 {
     public static void main(String[] args) {
+
+        for(int rowNum=1;rowNum <=12;rowNum++){
+            for(int N=1;N<=12;N++){
+                System.out.printf("%4d",N*rowNum);
+            
+            }
+            System.out.println();
+        }
         
     }
 }
