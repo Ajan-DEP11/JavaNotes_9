eb20523cf911544cbc4a497b2f0950f113c6f6a8
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-02 09:58:22 +0530

    Add scanner

--- IfStatement.java
+++ IfStatement.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class IfStatement {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        int x;
+        int y;
+        int z;
+
+       System.out.println("Enter x: ");
+       x=scanner.nextInt();
+       scanner.nextLine();
+
+        System.out.println("Enter y: ");
+        y=scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Enter z: ");
+        z=scanner.nextInt();
+        scanner.nextLine();
         
     }
 }
