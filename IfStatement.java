b43c6d6dbbc132e7b214b667658e9f17642aae49
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-02 10:08:29 +0530

    final commit

--- IfStatement.java
+++ IfStatement.java
@@ -7,17 +7,30 @@ public class IfStatement {
         int y;
         int z;
 
-       System.out.println("Enter x: ");
+        do{
+
+       System.out.print("Enter x: ");
        x=scanner.nextInt();
        scanner.nextLine();
 
-        System.out.println("Enter y: ");
+        System.out.print("Enter y: ");
         y=scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("Enter z: ");
+        System.out.print("Enter z: ");
         z=scanner.nextInt();
         scanner.nextLine();
+
+        if(x<y && y<z){
+            System.out.println(x +","+y+","+z);
+        }else if(x>z && z>y){
+            System.out.println(y +","+ z +","+x);
+        }else if(x<z && x>y){
+            System.out.println(y +","+ x +","+z);
+        }else{
+            System.out.println(z +","+ x +","+y);
+        }
+    }while(true);
         
     }
 }
