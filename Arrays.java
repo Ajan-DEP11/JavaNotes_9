95c65c6602be43dda71163ddfde9992eeeada880
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-13 23:26:41 +0530

    add index

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,13 @@
 public class Arrays {
     public static void main(String[] args) {
-        
+        int[] number = new int[5];
+
+        number[0] = 10;
+        number[1] = 20;
+        number[2] = 30;
+        number[3] = 40;
+        number[4] = 50;
+
+       System.out.println(number[1]);
     }
 }
