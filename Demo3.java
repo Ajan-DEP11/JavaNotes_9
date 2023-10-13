7d35c8c1eab8b7be2904a7a945b0ada3070bd1ba
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-14 02:02:09 +0530

    initial commit

---
+++ Demo3.java
@@ -0,0 +1,12 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = x;
+            x = 20;
+
+            System.out.println(x);
+            System.out.println(y);
+
+            System.out.println("---------------------------------");
+    }
+}
