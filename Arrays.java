34b8d9fb3e18c7f1260f79b6205e384bc1fd3f97
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-13 23:36:34 +0530

    print array

--- Arrays.java
+++ Arrays.java
@@ -8,6 +8,8 @@ public class Arrays {
         number[3] = 40;
         number[4] = 50;
 
-       System.out.println(number[1]);
+       for(int i=0;i< number.length;i++){
+        System.out.println(number[i]);
+       }
     }
 }
