4448d95f578ddaa4942a20d2b7088e9c5a89686e
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-14 02:18:48 +0530

    last commit

--- Demo3.java
+++ Demo3.java
@@ -8,5 +8,37 @@ public class Demo3 {
             System.out.println(y);
 
             System.out.println("---------------------------------");
-    }
+
+            int[] A ={10,20,30,40,50};
+            int[] B = A;
+
+            for(int i=0;i< A.length;i++){
+                System.out.print( A[i]+" ");
+            }
+
+            System.out.println();
+
+            for(int i=0;i< B.length;i++){
+                System.out.print(B[i] + " ");
+            }
+            System.out.println();
+            System.out.println("===================================");
+
+            A[0] = 100;
+            B[4] = 500;
+
+
+            for(int i=0;i< A.length;i++){
+                System.out.print( A[i]+" ");
+            }
+
+            System.out.println();
+
+            for(int i=0;i< B.length;i++){
+                System.out.print(B[i] + " ");
+            }
+            System.out.println();
+
+
+    } 
 }
