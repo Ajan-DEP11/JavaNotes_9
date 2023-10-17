f1543a35e52a8260e69c511fe04339e34f5694c2
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-17 18:27:07 +0530

    last commit

--- Demo4.java
+++ Demo4.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Demo4 {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        loop:
+       do {
+        int[] num = {1,3,-5,23,12,7,9,45,67,11};
+
+        System.out.print("Enter a number to search: ");
+        int search = scanner.nextInt();
+        scanner.nextLine();
+
+        for(int i=0;i<num.length;i++){
+            if(num[i]==search){
+                System.out.printf("Number found at %s \n", i+1);
+                continue loop;
+              
+             }
+                
+             
+            }
+            System.out.println("number not found");
+            //continue;
+        }while(true);
         
     }
 }
