e8c7baca3a3ccd88c125bc4afd49068ec8beea47
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-19 22:26:19 +0530

    final commit

--- Demo6.java
+++ Demo6.java
@@ -1,15 +1,37 @@
 import java.util.Scanner;
 
 public class Demo6 {
-    //private static final Scanner scanner =  new Scanner(System.in);
+    private static final Scanner scanner =  new Scanner(System.in);
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,4,5,7,54,76,-90,45,45,23,5,8,9,0,-12,1,3,4,5,3,9};
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    System.out.printf("duplicate find at %s\n",i+1);
+
+        
+        int search;
+      do { 
+        System.out.print("Enter number to search: ");
+        search = scanner.nextInt();
+        scanner.nextLine();
+
+        int duplicates=0;
+        for(int i=0;i<nums.length;i++){
+         
+                if(nums[i]==search){
+                    duplicates++;
                 }
-            }
+            
         }
+          if(duplicates> 1){
+            System.out.printf("Number of duplicates: %s \n",duplicates);
+        }else if(duplicates==1){
+            System.out.println("No duplicates");
+        }else{
+            System.out.println(search+ "number not found");
+        }
+      
+
+        }while(true);
+
+        
+      
     }
 }
