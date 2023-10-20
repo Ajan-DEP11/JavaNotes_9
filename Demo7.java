421914808c2bb155b7b6cffd2ae4bb244f7e3692
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-20 23:56:47 +0530

    last commit

--- Demo7.java
+++ Demo7.java
@@ -9,11 +9,28 @@ public class Demo7 {
         int insertion= scanner.nextInt();
         scanner.nextLine();
 
-        int insertionIndex;
+        int insertionIndex=0;
         if(insertion> nums[nums.length-1]){
             insertionIndex = nums.length;
         }else{
-            
+            for(int i=0;i<nums.length;i++){
+                if(insertion<=nums[i]){
+                    insertionIndex = i;
+                    break;
+                }
+            }
+
         }
+        int[] temp = new int[nums.length +1];
+        for(int i=0;i<insertionIndex;i++){
+            temp[i] = nums[i];
+        }
+        temp[insertionIndex] = insertion;
+        for(int i= insertionIndex+1;i<temp.length;i++){
+            temp[i] = nums[i-1];
+        }
+        System.out.println(Array.to);
+
+
     }
 }
