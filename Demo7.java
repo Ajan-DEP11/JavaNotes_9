ef9e1fc552e14e57cbc7c3ba613868b80c162894
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-10-20 23:39:52 +0530

    second commit

--- Demo7.java
+++ Demo7.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Demo7 {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        int[] nums = {3, -2, -5, 7, -5, 4, 2, 4, 6, -3};
-        System.out.println();
+        int[] nums = {-10, -5, 1, 3, 7, 9};
+
+        System.out.print("Enter number to insert: ");
+        int insertion= scanner.nextInt();
+        scanner.nextLine();
+
+        int insertionIndex;
+        if(insertion> nums[nums.length-1]){
+            insertionIndex = nums.length;
+        }else{
+            
+        }
     }
 }
